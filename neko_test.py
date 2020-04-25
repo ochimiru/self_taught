db6d5277e78e5fe36c0e0b71663b1245ef5b6580
ochimiru <h.oikawa0730@gmail.com>
2020-04-25 14:22:50 +0900

    first push

---
+++ neko_test.py
@@ -0,0 +1,100 @@
+import tkinter
+import random
+
+cursor_x = 0
+cursor_y = 0
+mouse_x = 0
+mouse_y = 0
+mouse_c = 0
+
+# マウスを動かした時に実行する関数
+def mouse_move(e):
+    global mouse_x, mouse_y
+    mouse_x = e.x
+    mouse_y = e.y
+
+# マウスボタンをクリックした時に実行する関数
+def mouse_press(e):
+    global mouse_c
+    mouse_c = 1
+
+# マス目を管理する二次元リスト
+neko = [
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0]
+        ]
+
+# ネコを描写する関数
+def draw_neko():
+    for y in range(10):
+        for x in range(8):
+            if neko[y][x] > 0:
+                cvs.create_image(x*72+60, y*72+60, image=img_neko[neko[y][x]], tag="NEKO")
+
+# ネコが横に３つ並んだか調べる関数
+def yoko_neko():
+    for y in range(10):
+        for x in range(1,7):
+            if neko[y][x] > 0:
+                if neko[y][x-1] == neko[y][x] and neko[y][x+1] == neko[y][x]:
+                    neko[y][x-1] = 7
+                    neko[y][x] = 7
+                    neko[y][x+1] = 7
+
+# ネコを落下させる関数
+def drop_neko():
+    for y in range(8, -1, -1):
+        for x in range(8):
+            if neko[y][x] != 0 and neko[y+1][x] == 0:
+                neko[y+1][x] = neko[y][x]
+                neko[y][x] = 0
+
+# リアルタイム処理を行う関数
+def game_main():
+    global cursor_x, cursor_y, mouse_c
+    drop_neko()
+    if 24 <= mouse_x and mouse_x < 24+72*8 and 24 <= mouse_y and mouse_y < 24+72*10:
+        cursor_x = int((mouse_x-24) / 72)
+        cursor_y = int((mouse_y-24) / 72)
+        if mouse_c == 1:
+            mouse_c = 0
+            neko[cursor_y][cursor_x] = random.randint(1,6)
+    cvs.delete("CURSOR")
+    cvs.create_image(cursor_x*72+60, cursor_y*72+60, image=cursor, tag="CURSOR")
+    cvs.delete("NEKO")
+    draw_neko()
+    root.after(100, game_main)
+
+root = tkinter.Tk()
+root.title("クリックしてネコを置く")
+root.resizable(False, False)
+root.bind("<Motion>", mouse_move)
+root.bind("<ButtonPress>", mouse_press)
+cvs = tkinter.Canvas(root, width=912, height=768)
+cvs.pack()
+
+bg = tkinter.PhotoImage(file="neko_bg.png")
+cursor = tkinter.PhotoImage(file="neko_cursor.png")
+img_neko = [
+        None,
+        tkinter.PhotoImage(file="neko1.png"),
+        tkinter.PhotoImage(file="neko2.png"),
+        tkinter.PhotoImage(file="neko3.png"),
+        tkinter.PhotoImage(file="neko4.png"),
+        tkinter.PhotoImage(file="neko5.png"),
+        tkinter.PhotoImage(file="neko6.png"),
+        tkinter.PhotoImage(file="neko_niku.png")
+        ]
+
+cvs.create_image(456, 384, image=bg)
+game_main()
+root.mainloop()
+
