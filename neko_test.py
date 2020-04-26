808e9f5a54cbb35c80a38c80bc06a1bec80fcf0a
Your Name <you@example.com>
2020-04-26 11:09:26 +0900

    3つ以上並ぶと消える処理まで

--- neko_test.py
+++ neko_test.py
@@ -20,8 +20,10 @@ def mouse_press(e):
 
 # マス目を管理する二次元リスト
 neko = []
+check = []
 for i in range(10):
     neko.append([0,0,0,0,0,0,0,0])
+    check.append([0,0,0,0,0,0,0,0])
 
 # ネコを描写する関数
 def draw_neko():
@@ -30,23 +32,52 @@ def draw_neko():
             if neko[y][x] > 0:
                 cvs.create_image(x*72+60, y*72+60, image=img_neko[neko[y][x]], tag="NEKO")
 
-# ネコが横に３つ並んだか調べる関数
-def yoko_neko():
+# ネコが縦横斜めに３つ並んだか調べる関数
+# nekoリストをcheckリストにコピーする
+def check_neko():
     for y in range(10):
-        for x in range(1,7):
-            if neko[y][x] > 0:
-                if neko[y][x-1] == neko[y][x] and neko[y][x+1] == neko[y][x]:
+        for x in range(8):
+            check[y][x] = neko[y][x]
+
+# ネコが縦に3つ並んだ時の処理
+    for y in range(1,9):
+        for x in range(8):
+            if check[y][x] > 0:
+                if check[y-1][x] == check[y][x] and check[y+1][x] == check[y][x]:
+                    neko[y-1][x] = 7
+                    neko[y][x] = 7
+                    neko[y+1][x] = 7
+
+# ネコが横に3つ並んだ時の処理
+    for y in range(10):
+        for x in range(1, 7):
+            if check[y][x] > 0:
+                if check[y][x-1] == check[y][x] and check[y][x+1] == check[y][x]:
                     neko[y][x-1] = 7
                     neko[y][x] = 7
                     neko[y][x+1] = 7
 
+# ネコが斜めに3つ並んだ時の処理
+    for y in range(1, 9):
+        for x in range(1, 7):
+            if check[y][x] > 0:
+                # 左上と右下が3つ並んだら
+                if check[y-1][x-1] == check[y][x] and check[y+1][x+1] == check[y][x]:
+                    neko[y-1][x-1] == 7
+                    neko[y][x] = 7
+                    neko[y+1][x+1] == 7
+                # 左下と右上が3つ並んだら
+                if check[y+1][x-1] == check[y][x] and check[y-1][x+1] == check[y][x]:
+                    neko[y+1][x-1] = 7
+                    neko[y][x] = 7
+                    neko[y-1][x+1] = 7
 
 # リアルタイム処理を行う関数
 def game_main():
     global cursor_x, cursor_y, mouse_c
     if 660 <= mouse_x and mouse_x < 840 and 100 <= mouse_y and mouse_y < 160 and mouse_c ==1:
         mouse_c = 0
-        yoko_neko()
+        check_neko()
     if 24 <= mouse_x and mouse_x < 24+72*8 and 24 <= mouse_y and mouse_y < 24+72*10:
         cursor_x = int((mouse_x-24) / 72)
         cursor_y = int((mouse_y-24) / 72)
